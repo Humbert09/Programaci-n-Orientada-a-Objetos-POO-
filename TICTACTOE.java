c1b992161123ad55de5801c54f7aa107cb57ac6c
Humbert09 <113150226+Humbert09@users.noreply.github.com>
2023-02-22 21:49:13 -0600

    Tarea 3: Juego del Gato

---
+++ TICTACTOE.java
@@ -0,0 +1,9 @@
+public class TICTACTOE {
+    public static void main(String[] args) {
+        System.out.println("-----");
+        System.out.println("|   |");
+        System.out.println("|   |");
+        System.out.println("|   |");
+        System.out.println("-----");
+    }
+}
