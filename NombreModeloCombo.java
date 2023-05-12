8f9da3ab760081e6f337e07d17c0f063c30a9bbe
Humbert09 <113150226+Humbert09@users.noreply.github.com>
2023-05-11 19:29:26 -0600

    Tarea 14 _ ComboBox

---
+++ NombreModeloCombo.java
@@ -0,0 +1,55 @@
+package ico2208;
+
+import javax.swing.*;
+import javax.swing.event.ListDataListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class NombreModeloCombo implements ComboBoxModel {
+    ArrayList<String> datos;
+    String selected;
+
+
+    public NombreModeloCombo() {
+        datos = new ArrayList<>();
+    }
+
+    public NombreModeloCombo(ArrayList<String> datos) {
+        this.datos = datos;
+    }
+
+
+    @Override
+    public void setSelectedItem(Object anItem) {
+        this.selected = (String) anItem;
+    }
+
+    @Override
+    public Object getSelectedItem() {
+        return selected;
+    }
+
+    @Override
+    public int getSize() {
+        return datos.size();
+    }
+
+    @Override
+    public Object getElementAt(int i) {
+        return datos.get(i);
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener l) {
+
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+
+    }
+
+    public void agregarNombre(String nom){
+        datos.add(nom);
+    }
+}
